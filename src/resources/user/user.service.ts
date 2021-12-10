cf3a7d454f9864f157d110a1ffe28ce151d766a9
Waliks <walikslucas2007@hotmail.com>
2021-12-09 21:07:59 -0300

    Adding pix controllers and services, and user/me

--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -76,4 +76,19 @@ export default class UserService {
 
         return {accesToken: token}
     }
+
+    async me(user: Partial<User>){
+        const userRepository = getRepository(User)
+        const currentUser = await userRepository.findOne({where: {id: user.id}})
+
+        if(!currentUser){
+            throw new AppError('Usuário não encontrado', 401)
+        }
+
+        // @ts-expect-error ignora
+        delete currentUser.password
+
+        return currentUser
+        
+    }
 }
