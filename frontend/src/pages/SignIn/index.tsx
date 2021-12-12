92936851778cbf1d27f68df73a94cf352286405b
Waliks <walikslucas2007@hotmail.com>
2021-12-12 15:10:52 -0300

    creating frontend initial configurations and Card component

---
+++ frontend/src/pages/SignIn/index.tsx
@@ -0,0 +1,18 @@
+import {Wrapper, Background, InputContainer, Buttonontainer} from './styles'
+import background from '../../assets/images/background-login.jpg'
+import Card from '../../components/Card'
+
+const SignIn = () => {
+
+
+    return (
+        <Wrapper>
+            <Background image={background}/>
+            <Card width="403px">
+                Teste
+            </Card>
+        </Wrapper>
+    )
+}
+
+export default SignIn
